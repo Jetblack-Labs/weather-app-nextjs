04c3c79e897cba29fcd02a271db03cd284677a8d
Suraj Jha <suraj.jha98@icloud.com>
2023-05-21 21:48:38 -0700

    Disable 'react/no-unescaped-entities' ESLint rule in pages.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+"use client";
+// eslint-disable-next-line react/no-unescaped-entities
+
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+import CityPicker from "@/components/CityPicker";
 
 export default function Home() {
-  return (
-    <main className="text-6xl">
-      Let's go !!
-    </main>
-  )
+   return (
+      <div className="text-6xl min-h-screen bg-gradient-to-br from-[#85a2c3] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+         <Card className="max-w-4xl mx-auto">
+            <Text className="text-6xl font-bold text-center mb-10">
+               Weather AI
+            </Text>
+            <Subtitle className="text-xl text-center">
+               Powered by ChatGPT
+            </Subtitle>
+
+            <Divider className="mb-10 text-center" />
+            <Card className="text-xl bg-gradient-to-br from-[#85a2c3] to-[#183B7E]">
+               <CityPicker />
+            </Card>
+         </Card>
+      </div>
+   );
 }
